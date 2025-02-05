4cd9e95e84d40cfb6bf8377445f44df2d9c8ba46
judedcunha <judedcunha3117@gmail.com>
2025-02-05 09:33:20 -0500

    Fine tuning

--- script.py
+++ script.py
@@ -33,7 +33,7 @@ uploaded_file = st.file_uploader("Upload an image of a digit (3 or 7)", type=["p
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     
     image = transform(image).view(1, -1)  # Flatten image
     
